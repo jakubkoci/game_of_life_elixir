852150661e9721f29fd3f142f106efd5ccdf8884
Jakub Koci <jakub.koci@gmail.com>
2025-05-22 07:48:17 +0200

    Implement rules

--- lib/game_of_life.ex
+++ lib/game_of_life.ex
@@ -15,4 +15,16 @@ defmodule GameOfLife do
   def hello(name) do
     "Hello, #{name}!"
   end
+
+  def next_cell_state(current_state, count_of_living_neighbours) do
+    next_state =
+      case {current_state, count_of_living_neighbours} do
+        {:live, 2} -> :live
+        {:live, 3} -> :live
+        {:dead, 3} -> :live
+        _ -> :dead
+      end
+
+    next_state
+  end
 end
--- test/game_of_life_test.exs
+++ test/game_of_life_test.exs
@@ -2,7 +2,28 @@ defmodule GameOfLifeTest do
   use ExUnit.Case
   doctest GameOfLife
 
-  test "greets" do
+  test "Greets" do
     assert GameOfLife.hello("Joe") == "Hello, Joe!"
+  end
+
+  test "Any live cell with fewer than two live neighbours dies, as if by underpopulation." do
+    assert GameOfLife.next_cell_state(:live, 0) == :dead
+    assert GameOfLife.next_cell_state(:live, 1) == :dead
+  end
+
+  test "Any live cell with two or three live neighbours lives on to the next generation." do
+    assert GameOfLife.next_cell_state(:live, 2) == :live
+    assert GameOfLife.next_cell_state(:live, 3) == :live
+  end
+
+  test "Any live cell with more than three live neighbours dies, as if by overpopulation." do
+    assert GameOfLife.next_cell_state(:live, 4) == :dead
+    assert GameOfLife.next_cell_state(:live, 5) == :dead
+  end
+
+  test "Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction." do
+    assert GameOfLife.next_cell_state(:dead, 2) == :dead
+    assert GameOfLife.next_cell_state(:dead, 3) == :live
+    assert GameOfLife.next_cell_state(:dead, 4) == :dead
   end
 end
